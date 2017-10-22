616b7ba883ed032a69b227407577fc4c13f58591
Carlos Eduardo <carloseduardofox8@gmail.com>
2017-10-22 00:19:46 -0300

    Estética

--- imagemutils.c
+++ imagemutils.c
@@ -3,7 +3,6 @@
 #include "imagemutils.h"
 
 void pegarPixels(Imagem *img) {
-
     char aux[5] = "", *ptr;
     int i, j;
 
@@ -11,22 +10,23 @@ void pegarPixels(Imagem *img) {
         for(j = 0; j < img->largura; j++) {
             img->pixels[i][j].x = i;
             img->pixels[i][j].y = j;
-
             fgets(aux, 5, img->stream);
             img->pixels[i][j].r = strtol(aux, &ptr, 10);
             fgets(aux, 5, img->stream);
             img->pixels[i][j].g = strtol(aux, &ptr, 10);
             fgets(aux, 5, img->stream);
+
             img->pixels[i][j].b = strtol(aux, &ptr, 10);
         }
     }
 }
 
+
+
 Imagem abrirImagem(char *nome) {
     Imagem img;
     char aux[100];
     img.stream = fopen(nome, "r");
-
     if(img.stream == NULL) {
         fprintf(stderr, "Aconteceu algum problema ao abrir a imagem\ncaminho ou nome do arquivo incorreto\n");
         exit(1);
@@ -34,14 +34,13 @@ Imagem abrirImagem(char *nome) {
 
     fgets(aux, 100, img.stream);
     aux[strlen(aux) - 1] = '\0';
-
     if(strcmp(aux, "P3") == 0) {
         img.tipo = P3;
     }
 
     fgets(aux, 100, img.stream);
-    fgets(aux, 100, img.stream);
 
+    fgets(aux, 100, img.stream);
     img.altura = atoi(strtok(aux, " "));
     img.largura = atoi(strtok(NULL, " "));
     img.pixels = malloc(img.altura * sizeof(Pixel *));
@@ -49,15 +48,12 @@ Imagem abrirImagem(char *nome) {
     for(i = 0; i < img.altura; i++) {
         img.pixels[i] = malloc(img.largura * sizeof(Pixel));
     }
-
     if(img.pixels == NULL || img.pixels[0] == NULL) {
         fprintf(stderr, "O programa não conseguiu alocar memória suficiente para a imagem. Logo, teve que ser encerrado.");
         exit(1);
     }
 
     pegarPixels(&img);
-
     fclose(img.stream);
-
     return img;
 }
--- main.c
+++ main.c
@@ -1,9 +1,7 @@
 #include "imagemutils.h"
 
 int main(int argc, char **argv) {
-
     Imagem img = abrirImagem(argv[1]);
     printf("Altura: %d\tLargura: %d\n", img.altura, img.largura);
-
     return 0;
 }
